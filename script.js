73afa957520e50286d4f2e9bd8540501615ce491
mana-mama <122524277+mana-mama@users.noreply.github.com>
2024-05-27 15:11:03 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -33,3 +33,22 @@ function closeInfo(yo) {
         yo.parentElement.style.display = "none";
     }, 300);
 }
+
+function showPicOf(yo) {
+    let exm_pics = yo.parentElement.parentElement.childNodes[1].querySelectorAll("img");
+    let small_pics = yo.parentElement.querySelectorAll("img");
+
+    for (i = 0; i < exm_pics.length; i++) {
+        exm_pics[i].style.display = "none";
+        if (exm_pics[i].id == yo.id) {
+            exm_pics[i].style.display = "block";
+        }
+    }
+
+    for (i = 0; i < small_pics.length; i++) {
+        small_pics[i].classList.remove("active");
+        if (small_pics[i].id == yo.id) {
+            small_pics[i].classList.add("active");
+        }
+    }
+}
